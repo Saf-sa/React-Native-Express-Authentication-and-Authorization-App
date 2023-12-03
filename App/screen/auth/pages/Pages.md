5cce130995fec305876f3991b9301ef8b7c1d585
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-12-03 19:51:11 +0100

    add pages folder & start.js when login succes

---
+++ App/screen/auth/pages/Pages.md
@@ -0,0 +1,2 @@
+1- add  pages folder for all pages from the app for different App fonctionality 
+2- add start.js page after login succes
---
+++ App/screen/auth/pages/start.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const start = () => {
+  return (
+    <View>
+      <Text>start</Text>
+    </View>
+  )
+}
+
+export default start
