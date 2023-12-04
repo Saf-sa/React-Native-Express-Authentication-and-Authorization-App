bc773caeed23b0535011a19ba55a279b4d47000b
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-12-04 11:44:41 +0100

    setup App.js navigation to reach each pages

--- App/App.js
+++ App/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screen/auth/login";
+import Signup from "./screen/auth/signup";
+import Reset from "./screen/auth/reset";
+import Start from "./screen/Pages/start";
+import ResetPassword from "./screen/auth/resetPassword";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Reset" component={Reset} />
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="ResetPassword" component={ResetPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
