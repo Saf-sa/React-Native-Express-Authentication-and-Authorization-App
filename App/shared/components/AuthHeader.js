be0054da10993cb886bddee6ae498a0ed20fb49b
Salah SAFSAF <120387600+Saf-sa@users.noreply.github.com>
2023-12-12 12:25:46 +0100

    update resetLogin in AuthHeader.js

--- App/shared/components/AuthHeader.js
+++ App/shared/components/AuthHeader.js
@@ -1,45 +1,46 @@
- import { View, Text, StyleSheet } from 'react-native'
- import React from 'react'
- 
- const AuthHeader = ({ subtext,}) => {
-   return (
-     <View style={styles.header}>
-       <Text style={styles.mainText}>EXPENSE MANAGER</Text>
-       <Text style={styles.subtext}>{subtext}</Text>
-   
-     </View>
-   );
- };
- 
- export default AuthHeader
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
 
- const styles = StyleSheet.create({
+const AuthHeader = ({ subtext }) => {
+  return (
+    <LinearGradient
+      style={styles.header}
+      colors={["#0283a8", "#F7EF8A", "#0000FF", "#0283a8"]}
+      start={{ x: 0.5, y: 0.6 }}
+      end={{ x: 3, y: 2 }}
+    >
+      <View style={styles.header}>
+        <Text style={styles.mainText}>EXPENSE MANAGER</Text>
+        <Text style={styles.subtext}>{subtext}</Text>
+      </View>
+    </LinearGradient>
+  );
+};
 
-    header: {
-        flex: 0.5,
-        backgroundColor: '#0283a8',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+export default AuthHeader;
 
-    mainText: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: '#fff',
-        textAlign: 'center',
+const styles = StyleSheet.create({
+  header: {
+    flex: 0.5,
+    /*   backgroundColor: '#0283a8', */
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 7,
+  },
 
-       
-    },
+  mainText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
 
-    subtext: {
-        fontSize: 15,
-        fontWeight: 'bold',
-        color: '#fff',
-        textAlign: 'center',
-        marginTop: 30,
-       
-    },
-
-
-
- })
+  subtext: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 30,
+  },
+});
